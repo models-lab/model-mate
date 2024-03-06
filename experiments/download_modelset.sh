be60327e34e3f819f916452e3b7b3f35c204725b
Jesus <jesus.sanchez.cuadrado@gmail.com>
2024-03-06 23:29:21 +0000

    Download correct Xtext dataset

--- experiments/download_modelset.sh
+++ experiments/download_modelset.sh
@@ -8,4 +8,5 @@ cd dataset
 wget -O modelset-lines-dedup.txt   http://models-lab.inf.um.es/files/model-mate/dataset/modelset-lines-dedup.txt
 wget -O modelset-normal-dedup.json http://models-lab.inf.um.es/files/model-mate/dataset/modelset-normal-dedup.json
 wget -O modelset-token-dedup.txt   http://models-lab.inf.um.es/files/model-mate/dataset/modelset-token-dedup.txt
-wget -O xtext-mar-dups.txt         http://models-lab.inf.um.es/files/model-mate/dataset/xtext-mar-dups.txt
+wget -O xtext-mar-dup.txt          http://models-lab.inf.um.es/files/model-mate/dataset/xtext-mar-dup.txt
+wget -O xtext-mar-dedup.txt        http://models-lab.inf.um.es/files/model-mate/dataset/xtext-mar-dedup.txt
