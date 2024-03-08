d77479eb84583ccc29917f224e4da54c69a74749
Jesus <jesus.sanchez.cuadrado@gmail.com>
2024-03-08 09:17:09 +0000

    Towards a common inference API and a server using it

---
+++ src/mate-serve.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+
+from inference import TokenInference
+from server.app import FlaskModelMateApp
+
+if __name__ == '__main__':
+    # TODO: We may have several models served via a configuration file
+    parser = ArgumentParser(description='Server to expose recommender')
+    parser.add_argument('--model', required=True,
+                        help='Path to the model')
+    parser.add_argument('--debug', default=False, action='store_true',
+                        help='Show debugging information')
+
+    args = parser.parse_args()
+
+    token_inference = TokenInference(args.model)
+    app = FlaskModelMateApp(token_inference)
+    app.run()
