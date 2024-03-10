c58cbff6adf4a3211b6e3a177e485614e8bb9b87
Jesús Sánchez Cuadrado <jesusc@um.es>
2024-03-10 17:17:22 +0100

    Serve the model from GPU or CPU

--- src/inference.py
+++ src/inference.py
@@ -19,13 +19,14 @@ class EndOfFunctionCriteriaToken(StoppingCriteria):
         #    if decoded_generation.count(' ') > 1:
         #        return False
         #return True
-        
+
 
 
 class TokenInference:
-    def __init__(self, model):
+    def __init__(self, model, device='cuda'):
         self.model = AutoModelForCausalLM.from_pretrained(model, device_map="auto")
         self.tokenizer = AutoTokenizer.from_pretrained(model)
+        self.device = device
 
     def get_suggestions_next_token(self, input, num_suggestions, max_new_tokens=10, max_length=512):
         model = self.model
@@ -37,8 +38,8 @@ class TokenInference:
         with torch.no_grad():
             criteria = EndOfFunctionCriteriaToken(tokenizer, sample["input_ids"].shape[1])
             generated_sequences = model.generate(
-                input_ids=sample["input_ids"].cuda(),
-                attention_mask=sample["attention_mask"].cuda(),
+                input_ids=self.to_device(sample["input_ids"]),
+                attention_mask=self.to_device(sample["attention_mask"]),
                 do_sample=False,
                 max_new_tokens=max_new_tokens,
                 num_return_sequences=num_suggestions,
@@ -55,11 +56,15 @@ class TokenInference:
         for k, new_tokens in enumerate(generated_new_tokens):
             generated = tokenizer.decode(new_tokens, skip_special_tokens=True)
             data = generated.split(' ')
-            print(data)
-            if (len(data) <= 1):
+            if len(data) <= 1:
                 break
             suggestions.append(data[1])
         return suggestions
+
+    def to_device(self, v):
+        if self.device == 'cuda' and torch.cuda.is_available():
+            return v.cuda()
+        return v
 
     def generate_fragment(self, input, stop=None):
         if stop is None:
@@ -67,11 +72,14 @@ class TokenInference:
 
         result = []
         while True:
-            token = self.get_suggestions_next_token(input, 1)[0]
+            suggestions = self.get_suggestions_next_token(input, 1)
+            if len(suggestions) == 0:
+                break
+            token = suggestions[0]
             #print("T: ", token)
             if token in stop:
                 break
-            input = input + token
+            input = input + ' ' + token
             result.append(token)
 
         return result
--- src/mate-serve.py
+++ src/mate-serve.py
@@ -8,11 +8,13 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='Server to expose recommender')
     parser.add_argument('--model', required=True,
                         help='Path to the model')
+    parser.add_argument('--device', required=False, default='cuda',
+                        help='Device used to run the model')
     parser.add_argument('--debug', default=False, action='store_true',
                         help='Show debugging information')
 
     args = parser.parse_args()
 
-    token_inference = TokenInference(args.model)
+    token_inference = TokenInference(args.model, args.device)
     app = FlaskModelMateApp(token_inference)
     app.run()
