f9394551188d0e1f1387311ac726beb5c64e7659
Jesús Sánchez Cuadrado <jesusc@um.es>
2024-09-22 01:08:05 +0200

    Options to run the server on a specific host and port

--- src/mate-serve.py
+++ src/mate-serve.py
@@ -12,9 +12,13 @@ if __name__ == '__main__':
                         help='Device used to run the model')
     parser.add_argument('--debug', default=False, action='store_true',
                         help='Show debugging information')
+    parser.add_argument('--port', default=8080,
+                        help='Port to run the server')
+    parser.add_argument('--host', default='0.0.0.0',
+                        help='Host to run the server')
 
     args = parser.parse_args()
 
     token_inference = ModelInference(args.model, args.device)
-    app = FlaskModelMateApp(token_inference)
+    app = FlaskModelMateApp(token_inference, port=args.port, host=args.host)
     app.run()
